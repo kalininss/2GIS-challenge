3e4bee46d81a720bfb081b006e7302f98e5721fb
Kalinin.ss <kalinin.ss@hotmail.com>
2015-02-08 16:49:50 +0600

    dsfa

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,59 +1,42 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
-
-int N;
-int M;
-
-int amount = 0;
 
 int main()
 {
 	setlocale(LC_ALL, "rus");
 
+	int N;			//строки
+	int M;			//столбцы
+	int amount = 0;	//счетчик домов
+
 //ввод данных пользоватлем
 
 	cout << "Программа определения количества домов на карте" << endl << endl;
 	cout << "Введите количество строк N: ";
-		cin >> N;
+	cin >> N;
 	cout << "Введите количество символов в строке M: ";
-		cin >> M;
+	cin >> M;
 
 //объявление двумерного массива
 
-		bool **array = new bool*[M];
-	for (int count = 0; count < M; count++)
-		array[count] = new bool[N];
-	
+	int **array = new int*[N];
+	for (int count = 0; count < N; count++)
+		array[count] = new int[M];
+
 //заполнение массива данными
+	
+	string line;
 
 	for (int n = 0; n < N; n++)
 	{
-		cout << "Введите строку № " << n + 1 << ": " << endl;
+		cout << "Введите строку № " << n + 1 << ": ";
+		cin >> line;
 		for (int i = 0; i < M; i++)
-			{
-				char check;
-				cin >> check;
-				if (check == '*') array[n][i] = 1;
-				else array[n][i] = 0;
-			}	
-
-	}
-
-//создание массива групп
-
-	int **group = new int*[M];
-	for (int count = 0; count < M; count++)
-		group[count] = new int[N];
-
-//обнуление массива групп
-
-	for (int n = 0; n < N; n++)//проход по строкам
-	{
-		for (int i = 0; i < M; i++)//проход по символам в строке
-		{
-			if (array[n][i] == 0) group[n][i] = -1;//если клетка без дома группа == -1
-			else group[n][i] = 0;//дом == 0
+		{			
+			if (line[i] == '*') array[n][i] = -1;	//-1 = дом
+			else array[n][i] = 0;				// 0 = нет дома
 		}
 	}
 
@@ -63,39 +46,42 @@ int main()
 	{
 		for (int i = 0; i < M; i++)//проход по символам в строке
 		{
-			if (array[n][i] == 1)//если в клетке дом 
+			if (array[n][i] == -1)//если в клетке дом 
 			{
 				if ((i == 0) && (n == 0))//если первый элемент дом
-				{					
-					group[n][i] = ++amount;					
+				{
+					array[n][i] = ++amount;
 				}
 				else
 				{
 					if (n == 0)//если первая строка
 					{
-						if (group[n][i - 1] == -1) group[n][i] = ++amount;//если слева нет дома, то счетчик +1
-						else group[n][i] = group[n][i - 1];//иначе приравнивается группа						
+						if (array[n][i - 1] == 0) array[n][i] = ++amount;		//если слева нет дома, то счетчик +1
+						else array[n][i] = array[n][i - 1];						//иначе приравнивается группа						
 					}
-					else 
+					else
 					{
-						if (i == 0)
+						if (i == 0)//если первый столбец
 						{
-							if (group[n-1][i] == -1) group[n][i] = ++amount;//если СВЕРХУ нет дома, то счетчик +1
-							else group[n][i] = group[n-1][i];//иначе приравнивается группа
+							if (array[n - 1][i] == 0) array[n][i] = ++amount;	//если СВЕРХУ нет дома, то счетчик +1
+							else array[n][i] = array[n - 1][i];					//иначе приравнивается группа
 						}
 						else
 						{
-							if ((group[n - 1][i] != -1) || (group[n][i - 1] != -1))//если слева или сверху ЕСТЬ дом
+
+							if (array[n - 1][i] != 0)					//если сверху дом
 							{
-								if (group[n - 1][i] != -1) group[n][i] = group[n - 1][i];//если слева дом
-								if (group[n][i - 1] != -1) group[n][i] = group[n][i - 1];//если справа дом
+								array[n][i] = array[n - 1][i];			
+								if ((array[n][i - 1] != 0) && (array[n][i - 1] != array[n - 1][i])) amount--;	//если сверху и снизу дом с разными группами
 							}
-							else ++amount;//иначе счетчик увеличивается
+							else
+							{
+								if (array[n][i - 1] != 0) array[n][i] = array[n][i - 1];//если сверху дом
+								else amount++;//иначе счетчик увеличивается
+							}							 
 						}
 					}
-					
 				}
-				
 			}
 		}
 	}
@@ -112,4 +98,4 @@ int main()
 	delete[] array;
 	system("PAUSE");
 	return 0;
-} 
+}
